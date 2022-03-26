13c20c592d65491bd0db44c8bb4c5d5156c88c97
Zhuang Niu <niu-zhuang@foxmail.com>
2022-03-26 17:20:47 +0800

    part02

---
+++ src/instruction.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    /// Halt the program
+    HLT,
+    /// Illegal opcode
+    IGL,
+}
+
+impl From<u8> for Opcode {
+    fn from(v: u8) -> Self {
+        match v {
+            0 => Opcode::HLT,
+            _ => Opcode::IGL,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode,
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Instruction {
+        Instruction { opcode }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_hlt() {
+        let opcode = Opcode::HLT;
+        assert_eq!(opcode, Opcode::HLT);
+    }
+
+    #[test]
+    fn test_create_instruction() {
+        let instruction = Instruction::new(Opcode::HLT);
+        assert_eq!(instruction.opcode, Opcode::HLT);
+    }
+}
--- src/vm.rs
+++ src/vm.rs
@@ -1,22 +1,71 @@
+use crate::instruction::Opcode;
+
 pub struct VM {
-    registers: [i32; 32]
+    registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>,
 }
 
 impl VM {
-    pub fn new() -> VM  {
+    pub fn new() -> VM {
         VM {
-            registers: [0; 32]
+            registers: [0; 32],
+            program: vec![],
+            pc: 0,
+        }
+    }
+
+    pub fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+
+        self.pc += 1;
+        opcode
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            // 如果 pc(程序计数器) 超出 program 的长度，则结束
+            if self.pc >= self.program.len() {
+                break;
+            }
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("HLT encountered");
+                    break;
+                }
+                _ => {
+                    println!("Unrecognized opcode found! Terminating!");
+                }
+            }
         }
     }
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn test_create_vm(){
+    fn test_create_vm() {
         let test_vm = VM::new();
         assert_eq!(test_vm.registers[0], 0);
+    }
+
+    #[test]
+    fn test_opcode_hlt() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![0, 0, 0, 0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_opcode_igl() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![200, 200, 0, 0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 3);
     }
 }
