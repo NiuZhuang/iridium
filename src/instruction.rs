f52e8777b49a4deef879723639a8981b681fc140
Zhuang Niu <niu-zhuang@foxmail.com>
2022-03-27 09:07:47 +0800

    feat(part04): update comment for opcodes

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,15 +1,42 @@
+/// Opcodes for the [instruction set](https://gitlab.com/subnetzero/iridium/blob/master/docs/manual.adoc).
 #[derive(Debug, PartialEq)]
 pub enum Opcode {
+    /// LOAD $0 $1 $2
+    /// 
+    /// Combines the second and third operand fields into a u16 which is then loaded into the register.
     LOAD,
+    /// ADD $0 $1 $2
+    /// 
+    /// Adds the contents of registers specified in operand 1 and 2 and places the result in register 3.
     ADD,
+    /// SUB $0 $1 $2
+    /// 
+    /// Subtracts register 2 from register 1 and places the result in register 3
     SUB,
+    /// MUL $0 $1 $2
+    /// 
+    /// Multiplies the contents of registers specified in operand 1 and 2 and places the result in register 3.
     MUL,
+    /// DIV $0 $1 $2 
+    /// 
+    /// Divides the contents of registers in operand 1 and 2; results go in register 3. The remainder goes in the remainder field of the VM.
     DIV,
-    HLT,  // Halt
-    JMP,  // Jump
-    JMPB, // Jump backwards
-    JMPF, // Jump forwards
-    IGL,  // Illegal
+    /// Halts execution of the program.
+    HLT,
+    /// JMP $0 
+    /// 
+    /// Jumps directly to the address in the specified in the register.
+    JMP,
+    /// JMPB $0 
+    /// 
+    /// Relative jump backward by the number in the register.
+    JMPB, 
+    /// JMPB $0 
+    /// 
+    /// Relative jump forward by the number in the register.
+    JMPF,
+    /// Used if an illegal opcode got in to the bytecode. 
+    IGL, 
 }
 
 impl From<u8> for Opcode {
